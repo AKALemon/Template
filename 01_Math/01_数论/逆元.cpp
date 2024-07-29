38c660495e88805d824ef28a726f5957cd50629d
AKALemon <1443909115@qq.com>
2024-07-29 13:52:28 +0800

    inv

--- 01_Math/01_数论/逆元.cpp
+++ 01_Math/01_数论/逆元.cpp
@@ -1,9 +1,8 @@
 // p为质数， O(n)递推逆元，求多个逆元
-i64 n, p;
 i64 inv[N];
-void init() {
+void init(int n) {
     inv[1] = 1;
     for (int i = 2; i <= n; i++) {
-        inv[i]=(p - p / i) * inv[p % i] % p;
+        inv[i]=(P - P / i) * inv[P % i] % P;
     }
 }
