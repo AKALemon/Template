742e01d874075528a800657035a578d11e3a7265
AKALemon <1443909115@qq.com>
2024-10-23 17:04:42 +0800

    min25

--- 01_Math/01_数论/Min_25筛.cpp
+++ 01_Math/01_数论/Min_25筛.cpp
@@ -1,60 +1,30 @@
-#include<bits/stdc++.h>
-using namespace std;
-using i64 = long long;
-using db = double;
-constexpr int N = 1e6 + 50, LOGN = 30;
-constexpr i64 P = 1e9 + 7, inf = 1e9;
-
+// f(p^k) = p^k * (p^k - 1)
+// sum1 = f的1次项的质数前缀和
+// sum2 = f的2次项的质数前缀和
+// g1 = f的1次项的前缀和
+// g2 = f的2次项的前缀和
 int p[N], num;
 i64 pr[N];
 i64 sum1[N], sum2[N];
 i64 n, sqr;
-void init(int n) {
+i64 w[N], id1[N], id2[N], tot;
+i64 g1[N], g2[N];
+i64 inv2 = qpow(2, P - 2), inv6 = qpow(6, P - 2);
+void init() {
+    sqr = sqrt(n);
     p[1] = 1;
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <= sqr; i++) {
         if (!p[i]) {
             pr[++num] = i;
             sum1[num] = (sum1[num - 1] + i) % P;
             sum2[num] = (sum2[num - 1] + 1ll * i * i % P) % P;
         }
-        for (int j = 1; j <= num && pr[j] * i <= n; j++) {
+        for (int j = 1; j <= num && pr[j] * i <= sqr; j++) {
             p[i * pr[j]] = 1;
             if (i % pr[j] == 0) break;
         }
     }
-}
 
-i64 qpow(i64 a, i64 b) {
-    i64 res = 1;
-    while (b) {
-        if (b & 1) res = res * a % P;
-        b >>= 1;
-        a = a * a % P;
-    }
-    return res;
-}
-i64 w[N], id1[N], id2[N], tot;
-i64 g1[N], g2[N], inv2, inv6;
-
-i64 S(i64 x, int y) {
-    if (pr[y] >= x) return 0;
-    i64 k = x <= sqr ? id1[x] : id2[n / x];
-    i64 ans = (g2[k] - g1[k] + P - (sum2[y] - sum1[y]) + P) % P;
-    for (int i = y + 1; i <= num && pr[i] * pr[i] <= x; i++) {
-        i64 pe = pr[i];
-        for (int e = 1; pe <= x; e++, pe *= pr[i]) {
-            i64 xx = pe % P;
-            ans = (ans + xx * (xx - 1) % P * (S(x / pe, i) + (e != 1)) % P) % P;
-        }
-    }
-    return ans;
-}
-
-void solve(){
-    cin >> n;
-    sqr = sqrt(n);
-    inv2 = qpow(2, P - 2), inv6 = qpow(6, P - 2);
-    init(sqr);
     for (i64 l = 1, r; l <= n;l = r + 1) {
         r = n / (n / l);
         w[++tot] = n / l;
@@ -71,16 +41,24 @@ void solve(){
         g1[j] = (g1[j] - pr[i] * (g1[k] - sum1[i - 1] + P) % P + P) % P;
         g2[j] = (g2[j] - pr[i] * pr[i] % P * (g2[k] - sum2[i - 1] + P) % P + P) % P;
     }
+}
+
+i64 S(i64 x, int y) {
+    if (pr[y] >= x) return 0;
+    i64 k = x <= sqr ? id1[x] : id2[n / x];
+    i64 ans = (g2[k] - g1[k] + P - (sum2[y] - sum1[y]) + P) % P;
+    for (int i = y + 1; i <= num && pr[i] * pr[i] <= x; i++) {
+        i64 pe = pr[i];
+        for (int e = 1; pe <= x; e++, pe *= pr[i]) {
+            i64 xx = pe % P;
+            ans = (ans + xx * (xx - 1) % P * (S(x / pe, i) + (e != 1)) % P) % P;
+        }
+    }
+    return ans;
+}
+
+void solve() {
+    cin >> n;
+    init();
     cout << (S(n, 0) + 1) % P << "\n";
 }
-
-signed main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr), cout.tie(nullptr);
-    cout << setprecision(15) << fixed;
-    int t = 1;
-    //cin >> t;
-    while (t--) solve();
-    return 0;
-}
-
